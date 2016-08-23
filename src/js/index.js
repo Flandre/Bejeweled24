c32751d6c8f8a415ed9cacf184b040a54861a273
Flandre <flantan233@gmail.com>
2016-08-23 13:01:30 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ function init() {
   for (var i = 0; i < ROW * COLUMN; i++) {
     $('.main').append('<div class="block" data-in="' + i + '">' + Math.ceil(Math.random() * 9) + '</div>')
   }
-  $('.block').each(function (index, ele) {
+  $('.block').each(function (index) {
     $(this)
       .css({
         'top': Math.floor(index / COLUMN) * (WIDTH + MARGIN) + MARGIN + 'px',
@@ -24,7 +24,6 @@ function init() {
       count = 0;
       $('.main')
         .on('mousemove', function (e) {
-          console.log(e.pageX + ',' + e.pageY)
           var index = Math.floor((e.pageX - $('.main').offset().left) / 125) + COLUMN * Math.floor((e.pageY - $('.main').offset().top) / 125);
           if (!listHash[index] && count < 4) {
             listHash[index] = $('.block[data-in="' + index + '"]')
@@ -58,7 +57,9 @@ function init() {
             delay += 500;
           }
         } else {
-          $('.block').removeClass('active press')
+          $('.block')
+            .removeClass('active press')
+            .removeAttr('data-toggle');
           console.log("+1s");
           // 执行+1s操作
         }
@@ -76,7 +77,7 @@ function remove_block(ele) {
     });
   var hideBlockColumn = index % COLUMN;
   var hideBlockRow = Math.ceil(index / ROW);
-  $('.block').each(function (idx, ele) {
+  $('.block').each(function () {
     if ($(this).attr('data-in') % COLUMN == hideBlockColumn && Math.ceil($(this).attr('data-in') / ROW) < hideBlockRow) {
       $(this)
         .css({
