27c825031db64131e297529f8a12cb6681fa7401
Flandre <flantan233@gmail.com>
2016-08-24 15:51:08 +0800

    init page

---
+++ js/24.js
@@ -0,0 +1,173 @@
+function test(){
+	console.log("fulan is baka");
+}
+
+
+
+
+var goal  = 24;
+var equal=0.000001;
+
+function foo(arr){
+	for(var i=0;i<arr.length;i++){
+		arr[i]=parseInt(arr[i]);
+	}
+	return is24(arr);
+}
+
+var outputret=true;
+
+function is24(arr){
+	if(arr.length!=4){
+		alert('wrong arr');
+		return;
+	}
+	for(var j=0;j<arr.length;j++){
+		var tmp = new Array();
+		for(var k=0;k<arr.length;k++){
+			tmp[k]=arr[k];
+		}
+		tmp.splice(j,1);
+		if(can(tmp,arr[j],goal)){
+			
+			return true;
+		}
+	}
+	if(judge2x2(arr,12)){
+		return true;
+	}
+	if(judge2x2(arr,13)){
+		return true;
+	}
+	if(judge2x2(arr,14)){
+		return true;
+	}
+	return false;
+}
+
+function judge2x2(arr,type){
+	var p1;
+	var p2;
+	var p3;
+	var p4;
+	if(type==12){
+		p1=0;
+		p2=1;
+		p3=2;
+		p4=3;
+	}else if(type==13){
+		p1=0;
+		p2=2;
+		p3=1;
+		p4=3;
+	}else if(type==14){
+		p1=0;
+		p2=3;
+		p3=1;
+		p4=2;
+	}
+	var t12list = retlist(arr[p1],arr[p2]);
+	for(var i=0;i<t12list.length;i++){
+		if(can([arr[p3],arr[p4]],t12list[i],goal)){
+			var ret = t12list[i];
+			var last = arr[p2];
+			if(outputret){
+				if(i==0){
+					console.log((ret-last)  + "+" + last + "=" + ret);
+				}else if(i==1){
+					console.log((ret+last)  + "-" + last + "=" + ret);
+				}else if(i==2){
+					console.log((ret/last)  + "*" + last + "=" + ret);
+				}else if(i==3){
+					console.log((ret*last)  + "/" + last + "=" + ret);
+				}else if(i==4){
+					console.log(last  + "-" + (last-ret) + "=" + ret);
+				}else if(i==5){
+					console.log(last  + "/" + (last/ret) + "=" + ret);
+				}				
+			}
+			return true;
+		}
+	}
+}
+
+
+function eq(x,y){
+	if(Math.abs(x-y)<equal){
+		//console.log('true')
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function can(arr,last,ret){
+	if(arr.length==0){
+		return eq(last,ret);
+	}else{
+		var goallist = [ret+last,ret-last,ret*last,ret/last,last-ret,last/ret];
+		for(var i=0;i<goallist.length;i++){
+			for(var j=0;j<arr.length;j++){
+				var tmp = new Array();
+				for(var k=0;k<arr.length;k++){
+					tmp[k]=arr[k];
+				}
+				tmp.splice(j,1);
+				if(can(tmp,arr[j],goallist[i])){
+					if(outputret){
+						if(i==0){
+							console.log((ret+last)  + "-" + last + "=" + ret);
+						}else if(i==1){
+							console.log((ret-last)  + "+" + last + "=" + ret);
+						}else if(i==2){
+							console.log((ret*last)  + "/" + last + "=" + ret);
+						}else if(i==3){
+							console.log((ret/last)  + "*" + last + "=" + ret);
+						}else if(i==4){
+							console.log(last  + "-" + (last-ret) + "=" + ret);
+						}else if(i==5){
+							console.log(last  + "/" + (last/ret) + "=" + ret);
+						}						
+					}
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
+
+
+function retlist(a,b){
+	return [a+b,a-b,a*b,a/b,b-a,b/a];
+}
+
+
+function test24(){
+	var t1 = new Date().getTime();
+	var max=100;
+	for(var i=0;i<10000;i++){
+		var x = [Math.ceil(Math.random()*max),Math.ceil(Math.random()*max),Math.ceil(Math.random()*max),Math.ceil(Math.random()*max)];
+		is24(x);
+		if(i%1000==0){
+			console.log(i);
+		}
+	}
+	var t2 = new Date().getTime();
+	console.log("time cost:"+(t2-t1));
+}
+
+Array.prototype.unique = function()
+{
+	var n = {},r=[]; //n为hash表，r为临时数组
+	for(var i = 0; i < this.length; i++) //遍历当前数组
+	{
+		if (!n[this[i]]) //如果hash表中没有当前项
+		{
+			n[this[i]] = true; //存入hash表
+			r.push(this[i]); //把当前数组的当前项push到临时数组里面
+		}
+	}
+	return r;
+}
+
---
+++ js/index.js
@@ -0,0 +1,121 @@
+// 定义常量
+var COLUMN = 4,
+  ROW = COLUMN,
+  MARGIN = 7,
+  BORDER = 3,
+  MAIN_WIDTH = 500,
+  WIDTH = (MAIN_WIDTH - MARGIN) / COLUMN - MARGIN;
+
+
+// 初始化
+function init() {
+  // 定义游戏等级
+  var level;
+  for (var i = 0; i < ROW * COLUMN; i++) {
+    $('.main').append('<div class="block" data-in="' + i + '">' + Math.ceil(Math.random() * 9) + '</div>')
+  }
+  $('.block').each(function (index) {
+    $(this)
+      .css({
+        'top': Math.floor(index / COLUMN) * (WIDTH + MARGIN) + MARGIN + 'px',
+        'left': index % COLUMN * (WIDTH + MARGIN) + MARGIN + 'px',
+        'width': WIDTH + 'px',
+        'height': WIDTH + 'px',
+        'line-height': WIDTH - BORDER * 2 + 'px',
+        'font-size': WIDTH * .7 + 'px'
+      });
+  });
+  // 鼠标滑过事件，取4个数字
+  var listHash = {},
+    count = 0;
+  $('.main')
+    .on('mousedown', function () {
+      $('.block')
+        .removeClass('active press')
+        .removeAttr('data-toggle');
+      listHash = {};
+      count = 0;
+      $('.main')
+        .on('mousemove', function (e) {
+          var index = Math.floor((e.pageX - $('.main').offset().left) / (MAIN_WIDTH / COLUMN)) + COLUMN * Math.floor((e.pageY - $('.main').offset().top) / (MAIN_WIDTH / ROW));
+          if (!listHash[index] && count < 4) {
+            listHash[index] = $('.block[data-in="' + index + '"]')
+              .attr('data-toggle', count)
+              .addClass('active press')
+              .html();
+            count++;
+          }
+        })
+    })
+    .on('mouseup', function () {
+      $('.main').off('mousemove');
+      var listArr = [];
+      for (x in listHash) {
+        listArr.push(listHash[x]);
+      }
+      if (listArr.length < 4) {
+        console.log('未取满4个数字');
+        $('.block').removeClass('active press')
+      } else {
+        // 显示的数字
+        console.log(listArr.sort());
+        /*
+         * 传入判断函数foo(), 如果返回为true, 执行销毁, false不销毁(暂定)
+         */
+        if (foo(listArr)) {
+          //if (1) {
+          var delay = 0;
+          for (var i = 0; i < 4; i++) {
+            setTimeout("remove_block($('.block[data-toggle=\"" + i + "\"]'))", delay);
+            delay += 500;
+          }
+        } else {
+          $('.block')
+            .removeClass('active press')
+            .removeAttr('data-toggle');
+          console.log("+1s");
+          // 执行+1s操作
+        }
+      }
+    })
+}
+// 处理消除方块
+function remove_block(ele) {
+  var index = ele.attr('data-in');
+  ele
+    .css('z-index', '999')
+    .addClass('rotateOut animated')
+    .one('webkitAnimationEnd animationEnd', function () {
+      $(this).remove();
+    });
+  var hideBlockColumn = index % COLUMN;
+  var hideBlockRow = Math.ceil(index / ROW);
+  $('.block').each(function () {
+    if ($(this).attr('data-in') % COLUMN == hideBlockColumn && Math.ceil($(this).attr('data-in') / COLUMN) <= hideBlockRow) {
+      $(this)
+        .css({
+          'top': parseInt($(this).css('top').split('px')[0]) + WIDTH + MARGIN + 'px'
+        })
+        .attr('data-in', parseInt($(this).attr('data-in')) + COLUMN)
+    }
+  });
+  add_block(hideBlockColumn)
+}
+function add_block(col) {
+  var newBlock = '<div class="block" data-in="' + col + '" style="top:' + MARGIN + 'px;left:' + parseInt(col % ROW * (WIDTH + MARGIN) + MARGIN) + 'px;display:none;width:' + WIDTH + 'px;height:' + WIDTH + 'px;font-size:' + WIDTH * 0.7 + 'px;line-height:' + (WIDTH - BORDER * 2) + 'px">' + Math.ceil(Math.random() * 9) + '</div>';
+  $('.main').append(newBlock);
+  $('.block:hidden')
+    .show()
+    .addClass('zoomIn animated')
+    .one('webkitAnimationEnd animationEnd', function () {
+      $(this).removeClass('zoomIn animated')
+    });
+}
+
+
+// 为方块添加数字
+function addNumber(ele, level){
+
+}
+init();
+
